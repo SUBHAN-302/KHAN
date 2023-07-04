569e8d2793bdfef5e8850965435423bd87ec4e00
SUBHAN-302 <110691961+SUBHAN-302@users.noreply.github.com>
2023-07-05 00:30:06 +0500

    test.py

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
-import os, platform
+import os, platform, time, sys
+ 
 try:
+ 
     import requests
+ 
 except:
+ 
     os.system('pip install requests')
-import requests
+ 
+os.system('git pull --quiet 2>/dev/null')
+ 
 bit = platform.architecture()[0]
+ 
 if bit == '64bit':
     from SUBHAN import KHAN
+ 
+    KHAN()
+ 
+elif bit == '32bit':
+    from SUBHAN32 import KHAN
+ 
     KHAN()
