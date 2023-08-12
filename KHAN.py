270509cc6f982f4f62f8c5b0ab3180ba751a158e
SUBHAN-302 <110691961+SUBHAN-302@users.noreply.github.com>
2023-08-12 20:15:29 +0500

    KHAN.py

--- KHAN.py
+++ KHAN.py
@@ -1,23 +1,15 @@
 import os, platform, time, sys
- 
-try:
- 
-    import requests
- 
-except:
- 
+os.system('git pull')
+try: 
+    import requests 
+except: 
     os.system('pip install requests')
- 
 os.system('git pull --quiet 2>/dev/null')
- 
 bit = platform.architecture()[0]
- 
 if bit == '64bit':
+	print('\033[1;32m Congratulations Your Phone Supported This Tools ')
     from SUBHAN import KHAN
- 
     KHAN()
- 
 elif bit == '32bit':
-    from SUBHAN32 import KHAN
- 
-    KHAN()
+    print('\033[1;31m Sorry 32 bit Not Supported This Tools')
+    exit()
