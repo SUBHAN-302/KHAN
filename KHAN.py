4b34f561f0e2abc3df26c431cabe930fbda7ece0
SUBHAN-302 <110691961+SUBHAN-302@users.noreply.github.com>
2023-08-12 20:18:54 +0500

    KHAN.py

--- KHAN.py
+++ KHAN.py
@@ -7,7 +7,7 @@ except:
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
 if bit == '64bit':
-	print('\033[1;32m Congratulations Your Phone Supported This Tools ')
+    print('\033[1;32m Congratulations Your Phone Supported This Tools ')
     from SUBHAN import KHAN
     KHAN()
 elif bit == '32bit':
